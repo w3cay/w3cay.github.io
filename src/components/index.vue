57add75631e24f50a06bfdc2756885388c66b4f4
w3cay <anyang.chen@ushopal.com>
2017-08-06 11:57:29 +0800

    add cnzz

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,10 +2,10 @@
   <div class="hello">
     <h1>{{ title }}</h1>
     <ul class="items">
-      <li><a href="http://w3cay.com/blog/" target="_blank">博客</a></li>/
-      <li><a href="https://github.com/w3cay" target="_blank">代码</a></li>/
-      <li><a href="http://weibo.com/633012550" target="_blank">微博</a></li>/
-      <li><a href="https://www.zhihu.com/people/bright-chen" target="_blank">知乎</a></li>
+      <li><a href="http://w3cay.com/blog/" onclick="_czc.push(['_trackEvent', '链接', '点击', '博客','1','blog']);" target="_blank">博客</a></li>/
+      <li><a href="https://github.com/w3cay" onclick="_czc.push(['_trackEvent', '链接', '点击', '代码','1','github']);"  target="_blank">代码</a></li>/
+      <li><a href="http://weibo.com/633012550" onclick="_czc.push(['_trackEvent', '链接', '点击', '微博','1','weibo']);"  target="_blank">微博</a></li>/
+      <li><a href="https://www.zhihu.com/people/bright-chen" onclick="_czc.push(['_trackEvent', '链接', '点击', '知乎','1','zhihu']);"  target="_blank">知乎</a></li>
       <!-- <li><a href="http://w3cay.com/resume/" target="_blank">简历</a></li> -->
     </ul>
   </div>
