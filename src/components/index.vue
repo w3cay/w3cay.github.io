fd54d054972795354eb0a2e23eda23c3734e1caa
w3cay <anyang.chen@ushopal.com>
2017-12-17 18:26:56 +0800

    change blog lin

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,15 +2,22 @@
   <div class="hello">
     <h1>{{ title }}</h1>
     <ul class="items">
-      <li><a href="http://w3cay.com/blog/" onclick="_czc.push(['_trackEvent', '链接', '点击', '博客','1','blog']);" target="_blank">博客</a></li>/
-      <li><a href="https://github.com/w3cay" onclick="_czc.push(['_trackEvent', '链接', '点击', '代码','1','github']);"  target="_blank">代码</a></li>/
-      <li><a href="http://weibo.com/633012550" onclick="_czc.push(['_trackEvent', '链接', '点击', '微博','1','weibo']);"  target="_blank">微博</a></li>/
-      <li><a href="https://www.zhihu.com/people/bright-chen" onclick="_czc.push(['_trackEvent', '链接', '点击', '知乎','1','zhihu']);"  target="_blank">知乎</a></li>
+      <li><a href="https://github.com/w3cay/blog/issues"
+          onclick="_czc.push(['_trackEvent', '链接', '点击', '博客','1','blog']);"
+          target="_blank">博客</a></li>/
+      <li><a href="https://github.com/w3cay"
+          onclick="_czc.push(['_trackEvent', '链接', '点击', '代码','1','github']);"
+          target="_blank">代码</a></li>/
+      <li><a href="http://weibo.com/633012550"
+          onclick="_czc.push(['_trackEvent', '链接', '点击', '微博','1','weibo']);"
+          target="_blank">微博</a></li>/
+      <li><a href="https://www.zhihu.com/people/bright-chen"
+          onclick="_czc.push(['_trackEvent', '链接', '点击', '知乎','1','zhihu']);"
+          target="_blank">知乎</a></li>
       <!-- <li><a href="http://w3cay.com/resume/" target="_blank">简历</a></li> -->
     </ul>
   </div>
 </template>
-
 <script>
 /* global window,document */
 /* eslint no-param-reassign: ["error", { "props": false }]*/
@@ -22,13 +29,11 @@ export default {
       title: '安望云海',
     };
   },
-  computed: {
-  },
-  components: {
-  },
+  computed: {},
+  components: {},
 };
-</script>
 
+</script>
 <style lang='scss'>
 .items {
   a {
@@ -46,4 +51,5 @@ export default {
     // }
   }
 }
+
 </style>
